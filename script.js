fba16667f95f15dcebacf8e44b6a33adeae94648
NathanC90 <ymnchao@gmail.com>
2024-01-14 20:15:46 +0800

    Add revision function to remove the last character/digit

--- script.js
+++ script.js
@@ -21,6 +21,10 @@ operands.forEach(operand => {
     })
 })
 
+function display (firstNumber, secondNumber, operator) {
+
+}
+
 // Create a new function operate that takes an operator and 2 numbers and then calls one of the above functions on the numbers.
 // Create a basic HTML calculator with buttons for each digit, each of the above functions and an “Equals” key.
 // Do not worry about wiring up the JS just yet.
@@ -32,12 +36,19 @@ let clear = document.querySelector('.clear');
 clear.addEventListener('click', function() {
     firstNumber.textContent = '0';
 })
+
 // Create the functions that populate the display when you click the number buttons.
 // You should be storing the ‘display value’ in a variable somewhere for use in the next step.
 // Make the calculator work! You’ll need to store the first number and second number that are input into the calculator,
 // utilize the operator that the user selects, and then operate() on the two numbers when the user presses the “=” key.
 
-
+// Delete the last digit for revision
+let revision = document.querySelector('.delete');
+revision.addEventListener('click', function() {
+    let str = firstNumber.textContent;
+    str = str.slice(0, -1) // Remove the last character
+    firstNumber.textContent = str;
+})
 // You should already have the code that can populate the display, so once operate() has been called,
 // update the display with the ‘solution’ to the operation.
 // This is the hardest part of the project. You need to figure out how to store all the values and call the operate function with them.
