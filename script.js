427646b3a5e1e19533a8bc2199ffce5116ad8151
NathanC90 <ymnchao@gmail.com>
2024-01-15 18:33:00 +0800

    Create variables to store all the elements on the html

---
+++ script.js
@@ -0,0 +1,31 @@
+// Create variables to store all the elements on the html
+let currentValue = '';
+let previousValue = '';
+let operator = '';
+
+// display
+let display = document.querySelector('#display');
+
+// operands
+let numbers = document.querySelectorAll('.operand');
+
+// operators
+let operators = document.querySelectorAll('.operator');
+
+// AC button
+let clearBtn = document.querySelector('.clear');
+
+// +/- button
+let plusOrMinus = document.querySelector('.sign');
+
+// % button
+let percent = document.querySelector('.percent');
+
+// delete/revise button
+let deleteBtn = document.querySelector('.delete');
+
+// decimal button
+let decimalBtn = document.getElementById('decimal-btn');
+
+// = button
+let equals = document.getElementById('equals');
