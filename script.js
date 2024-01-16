fa1232ea505acaf2f960a849f0553444842d6b20
NathanC90 <ymnchao@gmail.com>
2024-01-16 08:06:48 +0800

    Remove some comments that are not so important

--- script.js
+++ script.js
@@ -3,33 +3,15 @@ let currentValue = '';
 let previousValue = '';
 let operator = '';
 
-// display
 let display = document.querySelector('#display');
-
-// operands
 let numbers = document.querySelectorAll('.operand');
-
-// operators
 let operators = document.querySelectorAll('.operator');
-
-// AC button
 let clearBtn = document.querySelector('.clear');
-
-// +/- button
 let plusOrMinus = document.querySelector('.sign');
-
-// % button
 let percent = document.querySelector('.percent');
-
-// delete/revise button
 let deleteBtn = document.querySelector('.delete');
-
-// decimal button
 let decimalBtn = document.getElementById('decimal-btn');
-
-// = button
 let equals = document.getElementById('equals');
-
 
 // handle numbers
 numbers.forEach(number => {
